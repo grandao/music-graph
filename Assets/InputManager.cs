70e0d3e7abf14a3e22746c5d33aa397921907e74
Pedro Arthur <bygrandao@gmail.com>
2018-12-24 16:15:47 -0200

    Add mouse input support & node decorations + materials

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -6,6 +6,7 @@ public class InputManager : MonoBehaviour
 {
     public GameObject node_prefab;
     public GameObject selection;
+    public GameObject drag;
 
     struct TouchStart {
         public int id;
@@ -18,6 +19,7 @@ public class InputManager : MonoBehaviour
     };
 
     Dictionary<int, TouchStart> touches = new Dictionary<int, TouchStart>();
+    Vector3 mouse_position = new Vector3();
 
     void Start()
     {
@@ -27,56 +29,66 @@ public class InputManager : MonoBehaviour
     void Update()
     {
         for (int i = 0; i < Input.touchCount; ++i)
+            DispatchEvent(Input.GetTouch(i));
+
+//Mouse touch simulation for debug
+#if true
+        TouchPhase phase = TouchPhase.Canceled;
+
+        if (Input.GetMouseButtonUp(0)) phase = TouchPhase.Ended;
+        else if (Input.GetMouseButtonDown(0)) phase = TouchPhase.Began;
+        else if (mouse_position != Input.mousePosition) phase = TouchPhase.Moved;
+
+        if (phase != TouchPhase.Canceled)
         {
-#if false
-            if (Input.GetTouch(i).phase == TouchPhase.Began)
-            {
-                Vector2 pos = Input.GetTouch(i).position;
-                Debug.Log(string.Format("Touch({2}) begin at ({0}, {1})", pos.x, pos.y, i));
-                //clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
-
-                Ray ray = Camera.main.ScreenPointToRay(pos);
-                Instantiate(node_prefab, ray.origin, Quaternion.identity);
-
-            } else if (Input.GetTouch(i).phase == TouchPhase.Moved)
-            {
-                Vector2 dp = Input.GetTouch(i).deltaPosition;
-                Debug.Log(string.Format("Touch({2}) moved by ({0}, {1})", dp.x, dp.y, i));
-            }
+            Touch t = new Touch();
+            t.position = Input.mousePosition;
+            t.phase = phase;
+            t.deltaPosition = Input.mousePosition - mouse_position;
+            DispatchEvent(t);
+        }
 #endif
-            Touch t = Input.GetTouch(i);
-            switch (t.phase)
-            {
-                case TouchPhase.Began:
-                    touches[t.fingerId] = new TouchStart(t.fingerId, t.position);
-                    Ray ray = Camera.main.ScreenPointToRay(t.position);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit))
-                        selection = hit.transform.gameObject;
-                    else
-                        selection = null;
-                    if (selection != null) Debug.Log("Got Something!");
-                    OnBegin(t);
-                    break;
-                case TouchPhase.Moved:
-                    if (selection != null)
-                        OnDrag(t, selection);
-                    else
-                        OnMove(t);
-                    break;
-                case TouchPhase.Ended:
-                case TouchPhase.Canceled:
-                    OnEnd(t);
-                    touches.Remove(t.fingerId);
-                    break;
-            }
+    }
+
+    void DispatchEvent(Touch t)
+    {
+        switch (t.phase)
+        {
+            case TouchPhase.Began:
+                touches[t.fingerId] = new TouchStart(t.fingerId, t.position);
+                Ray ray = Camera.main.ScreenPointToRay(t.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                    selection = hit.transform.gameObject;
+                else
+                    selection = null;
+                if (selection != null) Debug.Log(string.Format("Got {0}", selection.name));
+
+                drag = selection;
+
+                OnBegin(t);
+                break;
+            case TouchPhase.Moved:
+                if (drag != null)
+                    OnDrag(t, drag);
+                else
+                    OnMove(t);
+                break;
+            case TouchPhase.Ended:
+            case TouchPhase.Canceled:
+                drag = null;
+                OnEnd(t);
+                touches.Remove(t.fingerId);
+                break;
         }
     }
 
     void OnBegin(Touch t) { }
     void OnMove(Touch t) { }
     void OnDrag(Touch t, GameObject obj) {
-        obj.transform.position = Camera.main.ScreenPointToRay(t.position).origin;
+        Vector3 p = Camera.main.ScreenPointToRay(t.position).origin;
+        p.z = 0;
+        obj.transform.position = p;
     }
     void OnEnd(Touch t) { }
 }
