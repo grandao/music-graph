70c07be30b0a34ff7dbbcfb3ef92ba468e95a670
Pedro Arthur <bygrandao@gmail.com>
2019-03-29 14:39:05 -0300

    Update node layout and  assets

--- Assets/TimeSlider.cs
+++ Assets/TimeSlider.cs
@@ -8,8 +8,8 @@ using UnityEngine;
 */
 public class TimeSlider : MonoBehaviour
 {
-    Vector2 e1 = new Vector2(1, 1).normalized;
-    Vector2 e2 = new Vector2(1, -1).normalized;
+    Vector2 e1 = new Vector2(-1, 0);
+    Vector2 e2 = new Vector2(0, -1);
 
     public void Move(Vector2 position)
     {
@@ -18,11 +18,11 @@ public class TimeSlider : MonoBehaviour
         float s = Vector2.Dot(e2, v);
 
         float angle = Mathf.Atan2(s, c);
-        int i = (int)Mathf.Round(angle * (180.0f / Mathf.PI) / 45);
+        int i = (int)Mathf.Round(angle * (180.0f / Mathf.PI) / 30);
 
-        i = i < 0 ? i + 8 : i;
+        i = i < 0 ? i + 12 : i;
 
-        if (i > 5) i = 0;
+        if (i > 7) i = 0;
         else if (i > 3) i = 3;
 
 
@@ -46,11 +46,14 @@ public class TimeSlider : MonoBehaviour
         if (i < 0) i = 0;
         if (i > 3) i = 3;
 
-        float c = Mathf.Cos(i * 45 * Mathf.PI / 180.0f);
-        float s = Mathf.Sin(i * 45 * Mathf.PI / 180.0f);
+        float c = Mathf.Cos(i * 30 * Mathf.PI / 180.0f);
+        float s = Mathf.Sin(i * 30 * Mathf.PI / 180.0f);
 
-        Vector2 p = 1.2f * (e1 * c + e2 * s);
+        Vector2 p = 0.8f * (e1 * c + e2 * s);
         gameObject.transform.localPosition = p;
+
+        //base obj rot is (45, -90, 90)
+        gameObject.transform.rotation = Quaternion.Euler(-45 + 30 * i, -90, 90);
     }
 
     public void UpdatePosition()
